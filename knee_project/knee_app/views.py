a24db4d57993da9128aab2def6b968449751589f
gomez2608 <s.gomeza1@uniandes.edu.co>
2024-07-13 18:09:03 -0500

    First version working

--- knee_project/knee_app/views.py
+++ knee_project/knee_app/views.py
@@ -5,11 +5,19 @@ import google.generativeai as genai
 import os
 from PIL import Image
 import tensorflow as tf
+import numpy as np
+import json
+from dotenv import load_dotenv
+load_dotenv()
 
-GEMINI_KEY = os.environ.get("GEMINI_KEY")
-#genai.configure(api_key=GEMINI_KEY)
-img_model = tf.keras.models.load_model('modelo.keras')
-#model = genai.GenerativeModel('gemini-1.5-flash-latest',system_instruction=f"you are my json interpeter, extract {' '.join(list(dict_cor.keys()))} from the text and return a json format according to these categories: {dict_cor}")
+print(os.getcwd())
+recomendations = json.load(open('recomendations.json',"r", encoding="utf8"))
+GEMINI_KEY = os.getenv("GEMINI_KEY")
+
+categories = ["doubtful", "mild", "moderate","normal", "severe"]
+genai.configure(api_key=GEMINI_KEY)
+img_model = tf.keras.applications.ResNet50(weights='imagenet')
+text_model = genai.GenerativeModel('gemini-1.5-flash-latest',system_instruction=f"You are a doctor assistant to knee arthritis detection, you receive a diagnostic that can be ['doubtful', 'mild', 'moderate', 'severe','normal'] and a probability of the diagnostic. Redact a message indicating the possible diagnostic according to the given categories, the probability and the next steps to follow in spanish without bold font.")
 
 @csrf_exempt
 def chatbot_response(request):
@@ -19,17 +27,20 @@ def chatbot_response(request):
         if 'image' in request.FILES:
             image_file = request.FILES['image']
             image = Image.open(image_file)
-            tensor = tf.convert_to_tensor(image)
-            prediction = img_model.predict(tensor)
-            print(prediction)
-        # Process the image here
-            return JsonResponse({'response': "Image received"}, status=200)
+            res_image = image.resize((224, 224))
+            
+            tensor = tf.convert_to_tensor(res_image)
+            prediction = img_model.predict(tensor[tf.newaxis, ...],verbose=0).flatten()[:5]
+            relevant_information = generate_response({"prediction":categories[np.argmax(prediction)],"probability":np.max(prediction),"recomendations":recomendations[categories[np.argmax(prediction)]]["recomendations"]})
+            message = generate_response(relevant_information)
+            return JsonResponse({'response': f"{message}"}, status=200)
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
-def generate_response(message):
-    #response = model.generate_content(message)
+def generate_response(information):
+    prompt = f"{information}"
+    response = text_model.generate_content(prompt).to_dict()["candidates"][0]["content"]["parts"][0]["text"]
     
-    return f""
+    return response
 
 def chat(request):
     return render(request, 'knee_app/home.html')
